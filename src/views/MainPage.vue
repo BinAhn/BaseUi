a050de16b9b4449e2fef8f4304b6eaed15fdf5bb
BinAhn <ahngunc@gmail.com>
2020-04-29 17:58:38 +0900

    add

---
+++ src/views/MainPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- <h1 class="main">MainPage</h1> -->
+
+    <div class="wrap">
+        <header class="header">
+            <h1 class="logo">위메프</h1>
+            <div class="info">홍길동</div>
+        </header>
+        <ul class="tabs">
+            <li class="tabs-item">
+				<a href="#" class="tabs-link">CS목록</a>
+			</li>
+			<li class="tabs-item">
+				<a href="#" class="tabs-link">CS등록</a>
+			</li>
+            <li class="tabs-item">
+				<a href="#" class="tabs-link">기타CS등록</a>
+			</li>
+            <li class="tabs-item">
+				<a href="#" class="tabs-link">기타CS목록</a>
+			</li>
+			
+        </ul>
+        <section class="contents">
+            <div class="content-item">대시보드1</div>
+            <div class="content-item">대시보드2</div>
+        </section>
+        <aside class="leftbar">
+            <ul class="menu">
+                <li class="menu-item">
+                    <a href="#" class="menu-link">기타문의</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">기타CS</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">CS문의</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">CS등록</a>
+                </li>
+            </ul>
+        </aside>
+        <footer class="footer">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores quod minima perspiciatis amet dolorum, esse odit minus natus voluptate cupiditate!
+        </footer>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import '../assets/css/default.css';
+@import '../assets/css/main.css';
+</style>
